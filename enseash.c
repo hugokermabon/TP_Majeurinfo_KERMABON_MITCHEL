c4a9025c437af6529e25236835c3c82454345131
KERMABON_Hugo <hugo.kermabon@ensea.fr>
2023-11-29 14:05:21 +0100

    question 1 mise à jour

--- enseash.c
+++ enseash.c
@@ -2,13 +2,13 @@
 #include <string.h>
 #include <unistd.h>
 
-#define BUFSIZE 1024
+#define BUFSIZE 1096
 
 int main() {
-    char buf[BUFSIZE] = "Bienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\nenseash %% ";
-
+    char buf[BUFSIZE] = "Bienvenue dans le Shell ENSEA. \nPour quitter, \ntapez 'exit'.\nenseash % \n";
     write(STDOUT_FILENO, buf, strlen(buf));
 
     return 0;
 }
+
 
