2a57b0257d7103a4d67b679cae82b7b54e12778a
maelmitc <mael.mitchel@ensea.fr>
2023-11-30 21:38:38 +0100

    Question 5 commentaire

--- enseash5.c
+++ enseash5.c
@@ -6,7 +6,7 @@
 #include <time.h>
 
 #define BUFSIZE 1096
-struct timespec start, end;
+struct timespec start, end;//We define start and end to measure the time between the beginning and the end of the process
 
 char buffer[BUFSIZE];
 char Fils_Termine[BUFSIZE];
@@ -30,16 +30,16 @@ int main() {
             exit(EXIT_FAILURE);
         }
 
-        clock_gettime(CLOCK_MONOTONIC, &start);
+        clock_gettime(CLOCK_MONOTONIC, &start);//We get the time of this line's execution just before the fork
         PID = fork();
 
         if (PID != 0) {
             wait(&status);
-            clock_gettime(CLOCK_MONOTONIC, &end);
+            clock_gettime(CLOCK_MONOTONIC, &end);//We get the time at the end of the child process
 
-            long seconds = end.tv_sec - start.tv_sec;
-            long nanoseconds = end.tv_nsec - start.tv_nsec;
-            long milliseconds = seconds * 1000 + nanoseconds / 1000000;
+            long seconds = end.tv_sec - start.tv_sec; //We calculate the time span in seconds between start and end
+            long nanoseconds = end.tv_nsec - start.tv_nsec;//Same with nanoseconds
+            long milliseconds = seconds * 1000 + nanoseconds / 1000000;//everything gets converted to milliseconds
 
               if (WIFEXITED(status)) {
                 sprintf(Fils_Termine,"[exit :%d|%dms]\n", WEXITSTATUS(status),milliseconds);
