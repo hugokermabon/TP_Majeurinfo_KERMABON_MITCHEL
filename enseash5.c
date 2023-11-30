7c94b772405859ef61a9eb114679db95c7010e75
maelmitc <mael.mitchel@ensea.fr>
2023-11-30 21:14:18 +0100

    Question 5

---
+++ enseash5.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <time.h>
+
+#define BUFSIZE 1096
+struct timespec start, end;
+
+char buffer[BUFSIZE];
+char Fils_Termine[BUFSIZE];
+char Fils_Arrete[BUFSIZE];
+
+int main() {
+    char buf[BUFSIZE] = "Bienvenue dans le Shell ENSEA. \nPour quitter, \ntapez 'exit'.\nenseash % \n";
+    write(STDOUT_FILENO, buf, strlen(buf));
+
+    while (1) {
+
+        write(1,"% ", strlen("% "));
+
+        int PID, status, commande;
+
+          // On utilise STDIN_FILENO pour lire depuis l'entrée standard
+        commande=read(STDIN_FILENO, buffer, BUFSIZE);//L'utilisateur écrit dans buffer
+        buffer[commande-1]='\0';
+        if (strcmp("exit",buffer)==0||commande==0){ //on compte les différences entre le buffer et le mot "exit"
+            write(1,"Bye, bye...", strlen("Bye, bye..."));
+            exit(EXIT_FAILURE);
+        }
+
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        PID = fork();
+
+        if (PID != 0) {
+            wait(&status);
+            clock_gettime(CLOCK_MONOTONIC, &end);
+
+            long seconds = end.tv_sec - start.tv_sec;
+            long nanoseconds = end.tv_nsec - start.tv_nsec;
+            long milliseconds = seconds * 1000 + nanoseconds / 1000000;
+
+              if (WIFEXITED(status)) {
+                sprintf(Fils_Termine,"[exit :%d|%dms]\n", WEXITSTATUS(status),milliseconds);
+                write(1,Fils_Termine,strlen(Fils_Termine));
+                } 
+            else if (WIFSIGNALED(status)) {
+                sprintf(Fils_Arrete,"[sign :%d|%dms]\n", WTERMSIG(status),milliseconds);
+                write(1,Fils_Arrete,strlen(Fils_Arrete));
+                }
+    }
+        
+        else {
+       
+            execlp(buffer,buffer, (char*)NULL); 
+            
+            if (strlen(buffer)==0){
+                execlp("date","date", (char*)NULL);
+            }
+
+            exit(EXIT_SUCCESS); // Utiliser EXIT_FAILURE en cas d'échec
+        }
+        
+    }
+
+    return 0;
+}
+
